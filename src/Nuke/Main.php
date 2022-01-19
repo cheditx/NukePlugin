b6fadcabc2273735f5bf1cc01516f32100340d7a
cheditx <66524955+cheditx@users.noreply.github.com>
2022-01-19 09:59:40 +0100

    Add files via upload

--- src/Nuke/Main.php
+++ src/Nuke/Main.php
@@ -19,10 +19,14 @@ class Main extends PluginBase{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		if ($command->getName() == "nuke") {
-			$this->nuke = true;
-			$sender->sendMessage($this->getConfig()->get("NukeActivated"));
-			$this->getScheduler()->scheduleRepeatingTask(new NukeTask($this, ($sender instanceof Player ? $sender : null)), 20);
-			return false;
+			if ($this->nuke == false) {
+				$this->nuke = true;
+				$sender->sendMessage($this->getConfig()->get("NukeActivated"));
+				$this->getScheduler()->scheduleRepeatingTask(new NukeTask($this, ($sender instanceof Player ? $sender : null)), 20);
+				return false;
+			} else {
+				$sender->sendMessage($this->getConfig()->get("NukeAlreadyActivated"));
+			}
 		}
 		return false;
 	}
